d5fd811e9fd0e01cc1b3f24ec19bbfcf50cdd973
cbnolok <claudiobng@gmail.com>
2023-10-19 18:39:35 +0200

    Changed grammar tester for CI, added a minimal scp file for testing.

--- .ci-commands/project/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-npx textmate-tester \
-    --textmateExtension scp-spherex \
-    --syntax "$CI_NIX_FOLDER/syntaxes/scp.tmLanguage.json" \
-    generate-specs --all
-	
-	# FIXME: we need: 
-	#--examples "$FORNIX_FOLDER/language_examples/"
